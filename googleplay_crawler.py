9a0b62719a06bf81df9dcc874beee8ae169dae2e
Hyunju1 <hh960305@naver.com>
2019-03-22 16:00:56 +0900

    add results

--- googleplay_crawler.py
+++ googleplay_crawler.py
@@ -6,7 +6,13 @@ import time
 
 driver = webdriver.Chrome('./chromedriver')
 
-driver.get('https://play.google.com/store/search?q=%EB%A9%94%EC%8B%A0%EC%A0%80&c=apps')
+# "메신저" 검색
+#driver.get('https://play.google.com/store/search?q=%EB%A9%94%EC%8B%A0%EC%A0%80&c=apps')
+# "푸시 알림" 검색
+#driver.get('https://play.google.com/store/search?q=%ED%91%B8%EC%8B%9C%20%EC%95%8C%EB%A6%BC&c=apps')
+# "notification" 검색
+driver.get('https://play.google.com/store/search?q=notification&c=apps')
+
 
 lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match=False
@@ -25,15 +31,14 @@ app = []
 org = []
 desc_summary = []
 desc = []
-star = []
+#star = []
 
 for element in soup.find_all('div', {'class':'card-content id-track-click id-track-impression'}):
 	url.append(domain+element.find('a', {"class": "title"}).get('href'))
 	app.append(element.find('a', {"class": "title"}).get('title'))
 	org.append(element.find('a', {"class": "subtitle"}).get('title'))
 	desc_summary.append(element.find('div', {"class": "description"}).text)
-    #desc.append(element.find('div', {"class": "wSaTQd"}).text)
-	star.append(element.find('div', {"class": "tiny-star star-rating-non-editable-container"}).get('aria-label').split()[1])
+	#star.append(element.find('div', {"class": "tiny-star star-rating-non-editable-container"}).get('aria-label').split()[2])
 
 for ur in url :
     driver.get(ur)
@@ -43,7 +48,7 @@ for ur in url :
 
 
 
-df = pd.DataFrame({'url':url, 'app':app, 'org':org, 'desc_summary':desc_summary, 'desc':desc,'star':star})
+df = pd.DataFrame({'url':url, 'app':app, 'org':org, 'desc_summary':desc_summary, 'desc':desc})
 
 print(len(df))
 df.to_csv('./App.csv', index=False, encoding='utf-8')
