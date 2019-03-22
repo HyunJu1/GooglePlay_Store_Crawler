58d654056641fb0cea2e1edb15a7b0637326b2a9
Hyunju1 <hh960305@naver.com>
2019-03-22 13:21:51 +0900

    add detail contents

--- googleplay_crawler.py
+++ googleplay_crawler.py
@@ -3,16 +3,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-# local path
-#path = '/Users/charlie/Desktop/GooglePlay/'
 
-# Install latest Chrome driver:
-# https://sites.google.com/a/chromium.org/chromedriver/downloads
 driver = webdriver.Chrome('./chromedriver')
-# Page url
+
 driver.get('https://play.google.com/store/search?q=%EB%A9%94%EC%8B%A0%EC%A0%80&c=apps')
 
-# crawl until the end page
 lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match=False
 while(match==False):
@@ -28,6 +23,7 @@ domain = 'https://play.google.com'
 url = []
 app = []
 org = []
+desc_summary = []
 desc = []
 star = []
 
@@ -35,10 +31,19 @@ for element in soup.find_all('div', {'class':'card-content id-track-click id-tra
 	url.append(domain+element.find('a', {"class": "title"}).get('href'))
 	app.append(element.find('a', {"class": "title"}).get('title'))
 	org.append(element.find('a', {"class": "subtitle"}).get('title'))
-	desc.append(element.find('div', {"class": "description"}).text)
+	desc_summary.append(element.find('div', {"class": "description"}).text)
+    #desc.append(element.find('div', {"class": "wSaTQd"}).text)
 	star.append(element.find('div', {"class": "tiny-star star-rating-non-editable-container"}).get('aria-label').split()[1])
 
-df = pd.DataFrame({'url':url, 'app':app, 'org':org, 'desc':desc, 'star':star})
+for ur in url :
+    driver.get(ur)
+    soup = BeautifulSoup(driver.page_source, 'html.parser',from_encoding='utf-8')
+    #for element in soup.find_all('div', {'class':'JHTxhe IQ1z0d'}):
+    desc.append(soup.find('div', {'jsname':'sngebd'}).text)
+
+
+
+df = pd.DataFrame({'url':url, 'app':app, 'org':org, 'desc_summary':desc_summary, 'desc':desc,'star':star})
 
 print(len(df))
 df.to_csv('./App.csv', index=False, encoding='utf-8')
